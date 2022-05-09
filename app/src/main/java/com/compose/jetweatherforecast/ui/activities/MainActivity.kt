be1ad2baf46a79306510d016c99dc5e9c453358b
Alin Babu <alinbabu2010@gmail.com>
2022-05-09 14:34:27 +0530

    Modify composable to add app navigation

--- app/src/main/java/com/compose/jetweatherforecast/ui/activities/MainActivity.kt
+++ app/src/main/java/com/compose/jetweatherforecast/ui/activities/MainActivity.kt
@@ -3,12 +3,16 @@ package com.compose.jetweatherforecast.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.compose.jetweatherforecast.ui.componets.WeatherNavigation
 import com.compose.jetweatherforecast.ui.theme.JetWeatherForecastTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,23 +23,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetWeatherForecastTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
-                }
+                WeatherApp()
             }
         }
     }
     
 }
 
+
+@Composable
+fun WeatherApp(){
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            WeatherNavigation()
+        }
+
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetWeatherForecastTheme {
-
+        WeatherApp()
     }
 }
