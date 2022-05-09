d621ceda75eee60bf45beadd0839a7db4b63ca1f
Alin Babu <alinbabu2010@gmail.com>
2022-05-09 14:00:34 +0530

    Move activity to new package and change its usages

--- app/src/main/java/com/compose/jetweatherforecast/ui/activities/MainActivity.kt
+++ app/src/main/java/com/compose/jetweatherforecast/ui/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.compose.jetweatherforecast
+package com.compose.jetweatherforecast.ui.activities
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
