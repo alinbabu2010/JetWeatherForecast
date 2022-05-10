19d5b98559adca6938be5d416e21c836827546a2
Alin Babu <alinbabu2010@gmail.com>
2022-05-10 12:29:07 +0530

    Modify main screen navigation and parameters for main screen composable

--- app/src/main/java/com/compose/jetweatherforecast/ui/navigation/WeatherNavigation.kt
+++ app/src/main/java/com/compose/jetweatherforecast/ui/navigation/WeatherNavigation.kt
@@ -1,12 +1,14 @@
 package com.compose.jetweatherforecast.ui.componets
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.compose.jetweatherforecast.ui.navigation.WeatherScreens
 import com.compose.jetweatherforecast.ui.screens.MainScreen
 import com.compose.jetweatherforecast.ui.screens.WeatherSplashScreen
+import com.compose.jetweatherforecast.ui.viewmodels.WeatherViewModel
 
 @Composable
 fun WeatherNavigation() {
@@ -14,12 +16,13 @@ fun WeatherNavigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = WeatherScreens.SplashScreen.name){
 
-        composable(WeatherScreens.SplashScreen.name){
+        composable(WeatherScreens.SplashScreen.name) {
             WeatherSplashScreen(navController)
         }
 
-        composable(WeatherScreens.MainScreen.name){
-            MainScreen(navController)
+        composable(WeatherScreens.MainScreen.name) {
+            val weatherViewModel = hiltViewModel<WeatherViewModel>()
+            MainScreen(navController, weatherViewModel)
         }
 
     }
--- app/src/main/java/com/compose/jetweatherforecast/ui/screens/MainScreen.kt
+++ app/src/main/java/com/compose/jetweatherforecast/ui/screens/MainScreen.kt
@@ -3,9 +3,10 @@ package com.compose.jetweatherforecast.ui.screens
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import com.compose.jetweatherforecast.ui.viewmodels.WeatherViewModel
 
 @Composable
-fun MainScreen(navController: NavHostController) {
+fun MainScreen(navController: NavHostController, weatherViewModel: WeatherViewModel) {
 
     Text("Main Screen")
 
