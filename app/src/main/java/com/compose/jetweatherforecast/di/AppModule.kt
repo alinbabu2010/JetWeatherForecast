8d4fb9a7751cec83c9befab28262f2122f460408
Alin Babu <alinbabu2010@gmail.com>
2022-05-10 12:24:58 +0530

    Add retrofit providers in AppModule.kt

--- app/src/main/java/com/compose/jetweatherforecast/di/AppModule.kt
+++ app/src/main/java/com/compose/jetweatherforecast/di/AppModule.kt
@@ -1,4 +1,46 @@
 package com.compose.jetweatherforecast.di
 
+import com.compose.jetweatherforecast.BuildConfig
+import com.compose.jetweatherforecast.network.WeatherAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
 class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOpenWeatherApi(okHttpClient: OkHttpClient): WeatherAPI {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(WeatherAPI::class.java)
+    }
+
+
 }
