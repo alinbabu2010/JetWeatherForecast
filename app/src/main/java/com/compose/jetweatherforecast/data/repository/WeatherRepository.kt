9c971ed85e30a9b75e4cef17ea119daba683bfd1
Alin Babu <alinbabu2010@gmail.com>
2022-05-10 16:01:14 +0530

    Modify repository and viewModel function implementation

--- app/src/main/java/com/compose/jetweatherforecast/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/compose/jetweatherforecast/data/repository/WeatherRepository.kt
@@ -1,19 +1,18 @@
 package com.compose.jetweatherforecast.data.repository
 
 import com.compose.jetweatherforecast.data.model.Weather
-import com.compose.jetweatherforecast.data.wrappers.Resource
 import com.compose.jetweatherforecast.data.wrappers.ResponseWrapper
 import com.compose.jetweatherforecast.network.WeatherAPI
-import com.compose.jetweatherforecast.network.callWithExceptionHandling
 import javax.inject.Inject
 
 class WeatherRepository @Inject constructor(private val weatherAPI: WeatherAPI) {
 
-    fun getWeather(cityQuery: String): Resource<Weather?> {
-        return when (val response = weatherAPI.getWeather(cityQuery).callWithExceptionHandling()) {
-            is ResponseWrapper.Error -> Resource.error(response.error)
-            is ResponseWrapper.Failure -> Resource.failure(response.exception)
-            is ResponseWrapper.Success -> Resource.success(response.data)
+    suspend fun getWeather(cityQuery: String): ResponseWrapper<Weather> {
+        return try {
+            val response = weatherAPI.getWeather(cityQuery)
+            ResponseWrapper.Success(response)
+        } catch (exception: Exception) {
+            ResponseWrapper.Error(exception)
         }
     }
 
--- app/src/main/java/com/compose/jetweatherforecast/ui/viewmodels/WeatherViewModel.kt
+++ app/src/main/java/com/compose/jetweatherforecast/ui/viewmodels/WeatherViewModel.kt
@@ -1,14 +1,11 @@
 package com.compose.jetweatherforecast.ui.viewmodels
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.compose.jetweatherforecast.data.model.Weather
 import com.compose.jetweatherforecast.data.repository.WeatherRepository
 import com.compose.jetweatherforecast.data.wrappers.Resource
+import com.compose.jetweatherforecast.data.wrappers.ResponseWrapper
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,22 +13,14 @@ class WeatherViewModel @Inject constructor(
     private val repository: WeatherRepository
 ) : ViewModel() {
 
-    var data = mutableStateOf<Resource<Weather?>>(Resource.loading())
-
-    init {
-        loadWeather()
-    }
-
-    private fun loadWeather() {
-        getWeather("Se")
-    }
-
-    fun getWeather(city: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            if (city.isBlank()) return@launch
-            data.value = repository.getWeather(city)
+    suspend fun getWeather(city: String): Resource<Weather> {
+        return when (val response = repository.getWeather(city)) {
+            is ResponseWrapper.Error -> Resource.error(response.exception)
+            is ResponseWrapper.Success -> {
+                if (response.data != null) Resource.success(response.data)
+                else Resource.empty()
+            }
         }
     }
-
 
 }
