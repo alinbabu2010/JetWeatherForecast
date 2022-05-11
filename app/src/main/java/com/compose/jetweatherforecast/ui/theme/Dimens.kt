2175e5bd76e11b239315cad173bbbb8f83332366
Alin Babu <alinbabu2010@gmail.com>
2022-05-11 09:51:44 +0530

    Add weather main screen tool bar

--- app/src/main/java/com/compose/jetweatherforecast/ui/theme/Dimens.kt
+++ app/src/main/java/com/compose/jetweatherforecast/ui/theme/Dimens.kt
@@ -1,6 +1,9 @@
 package com.compose.jetweatherforecast.ui.theme
 
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val dimenZeroDp = 0.dp
 
 val splashSurfaceSize = 330.dp
 val splashIconSize = 95.dp
@@ -9,3 +12,7 @@ val dimenBorderWidth = 2.dp
 
 val dimenPadding1 = 1.dp
 val dimenPadding15 = 15.dp
+
+val elevation5Dp = 5.dp
+
+val dimenFontSize15 = 15.sp
---
+++ app/src/main/java/com/compose/jetweatherforecast/ui/widgets/WeatherAppBar.kt
@@ -0,0 +1,62 @@
+package com.compose.jetweatherforecast.ui.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.navigation.NavController
+import com.compose.jetweatherforecast.ui.theme.dimenFontSize15
+import com.compose.jetweatherforecast.ui.theme.dimenZeroDp
+
+@Composable
+fun WeatherAppBar(
+    title: String = "Title",
+    icon: ImageVector? = null,
+    isMainScreen: Boolean = true,
+    elevation: Dp = dimenZeroDp,
+    navController: NavController,
+    onAddActionClicked: () -> Unit = {},
+    onButtonClicked: () -> Unit = {}
+) {
+
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                color = MaterialTheme.colors.onSecondary,
+                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = dimenFontSize15)
+            )
+        },
+        backgroundColor = Color.Transparent,
+        navigationIcon = {
+            if (icon != null) {
+                Icon(imageVector = icon,
+                    contentDescription = null,
+                    tint = MaterialTheme.colors.onSecondary,
+                    modifier = Modifier.clickable { onButtonClicked.invoke() })
+            }
+        },
+        elevation = elevation,
+        actions = {
+            if (isMainScreen) {
+                IconButton(onClick = { }) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
+                }
+                IconButton(onClick = { }) {
+                    Icon(imageVector = Icons.Default.MoreVert, contentDescription = "More Icon")
+                }
+            } else Box() {
+
+            }
+        })
+
+}
