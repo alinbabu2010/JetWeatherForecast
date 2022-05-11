5b55b7800271231a4ae05323c26bb5afecd1cfd7
Alin Babu <alinbabu2010@gmail.com>
2022-05-11 09:53:20 +0530

    Modify main screen to add app bar and refactor code

--- app/src/main/java/com/compose/jetweatherforecast/ui/screens/MainScreen.kt
+++ app/src/main/java/com/compose/jetweatherforecast/ui/screens/MainScreen.kt
@@ -1,13 +1,56 @@
 package com.compose.jetweatherforecast.ui.screens
 
+import android.util.Log
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
 import androidx.navigation.NavHostController
+import com.compose.jetweatherforecast.data.model.Weather
+import com.compose.jetweatherforecast.data.wrappers.Resource
+import com.compose.jetweatherforecast.data.wrappers.Resource.Status.*
+import com.compose.jetweatherforecast.ui.theme.elevation5Dp
 import com.compose.jetweatherforecast.ui.viewmodels.WeatherViewModel
+import com.compose.jetweatherforecast.ui.widgets.WeatherAppBar
+import com.compose.jetweatherforecast.utils.Constants
 
 @Composable
 fun MainScreen(navController: NavHostController, weatherViewModel: WeatherViewModel) {
 
-    Text("Main Screen")
+    val weatherData = produceState<Resource<Weather?>>(
+        initialValue = Resource.loading()
+    ) {
+        value = weatherViewModel.getWeather("Seattle")
+    }.value
+
+    when (weatherData.status) {
+        SUCCESS -> weatherData.data?.let { MainScaffold(it, navController) }
+        LOADING -> CircularProgressIndicator()
+        ERROR -> Text(text = weatherData.message.toString())
+        EMPTY_RESPONSE -> Text(text = Constants.NOT_FOUND)
+    }
+
+}
+
+@Composable
+fun MainScaffold(weather: Weather, navController: NavHostController) {
+
+    Scaffold(topBar = {
+        WeatherAppBar(
+            title = "${weather.city.name}, ${weather.city.country}",
+            navController = navController,
+            elevation = elevation5Dp
+        ) {
+            Log.d("TAG", "MainScaffold: Button Clicked")
+        }
+    }) {
+        MainContent(weather)
+    }
+
+}
+
+@Composable
+fun MainContent(weather: Weather) {
 
 }
