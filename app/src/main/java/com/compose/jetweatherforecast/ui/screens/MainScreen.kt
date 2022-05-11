7369dd464b9ed4dcfbed6267ee7c89a042db4ba1
Alin Babu <alinbabu2010@gmail.com>
2022-05-11 13:57:08 +0530

    Add main screen top title and circular view to show temperature

--- app/src/main/java/com/compose/jetweatherforecast/ui/screens/MainScreen.kt
+++ app/src/main/java/com/compose/jetweatherforecast/ui/screens/MainScreen.kt
@@ -2,9 +2,14 @@ package com.compose.jetweatherforecast.ui.screens
 
 import android.util.Log
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavHostController
 import com.compose.jetweatherforecast.data.model.Weather
 import com.compose.jetweatherforecast.data.wrappers.Resource
@@ -12,7 +17,10 @@ import com.compose.jetweatherforecast.data.wrappers.Resource.Status.*
 import com.compose.jetweatherforecast.ui.theme.*
 import com.compose.jetweatherforecast.ui.viewmodels.WeatherViewModel
 import com.compose.jetweatherforecast.ui.widgets.WeatherAppBar
+import com.compose.jetweatherforecast.ui.widgets.WeatherStateImage
 import com.compose.jetweatherforecast.utils.Constants
+import com.compose.jetweatherforecast.utils.formatDate
+import com.compose.jetweatherforecast.utils.formatDecimals
 
 @Composable
 fun MainScreen(navController: NavHostController, weatherViewModel: WeatherViewModel) {
@@ -20,7 +28,7 @@ fun MainScreen(navController: NavHostController, weatherViewModel: WeatherViewMo
     val weatherData = produceState<Resource<Weather?>>(
         initialValue = Resource.loading()
     ) {
-        value = weatherViewModel.getWeather("Seattle")
+        value = weatherViewModel.getWeather(Constants.DEFAULT_CITY)
     }.value
 
     when (weatherData.status) {
@@ -51,5 +59,52 @@ fun MainScaffold(weather: Weather, navController: NavHostController) {
 
 @Composable
 fun MainContent(weather: Weather) {
+
+    val weatherItem = weather.list.first()
+    val weatherObject = weatherItem.weather.first()
+    val iconId = weatherObject.icon
+    val imageUrl = "https://openweathermap.org/img/wn/$iconId.png"
+
+    Column(
+        modifier = Modifier
+            .padding(mainColumnPadding)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = formatDate(weatherItem.dt),
+            style = MaterialTheme.typography.caption,
+            color = MaterialTheme.colors.onSecondary,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(mainDateTextPadding)
+        )
+
+        Surface(
+            modifier = Modifier
+                .padding(mainSurfacePadding)
+                .size(mainSurfaceSize),
+            shape = CircleShape,
+            color = Yellow100
+        ) {
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WeatherStateImage(imageUrl = imageUrl)
+                Text(
+                    text = formatDecimals(weatherItem.temp.day) + 0x00B0.toChar(),
+                    style = MaterialTheme.typography.h4,
+                    fontWeight = FontWeight.ExtraBold
+                )
+                Text(text = weatherObject.main, fontStyle = FontStyle.Italic)
+            }
+
+        }
+
+    }
+
 
 }
---
+++ app/src/main/java/com/compose/jetweatherforecast/ui/widgets/WeatherStateImage.kt
@@ -0,0 +1,19 @@
+package com.compose.jetweatherforecast.ui.widgets
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import coil.compose.rememberAsyncImagePainter
+import com.compose.jetweatherforecast.ui.theme.weatherStateImageSize
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+
+    Image(
+        painter = rememberAsyncImagePainter(imageUrl),
+        contentDescription = "Weather Image",
+        modifier = Modifier.size(weatherStateImageSize)
+    )
+
+}
