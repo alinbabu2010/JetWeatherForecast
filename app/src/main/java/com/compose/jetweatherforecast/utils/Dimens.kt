dd44d97541ca0b73f02eb934ea7d5120c93b83aa
Alin Babu <alinbabu2010@gmail.com>
2022-05-12 12:49:27 +0530

    Add new string resources and variables for color and dimensions

--- app/src/main/java/com/compose/jetweatherforecast/utils/Dimens.kt
+++ app/src/main/java/com/compose/jetweatherforecast/utils/Dimens.kt
@@ -34,3 +34,14 @@ val sunSetRiseRowPaddingTop = 15.dp
 val sunSetRiseRowPaddingBottom = 6.dp
 val sunSetRiseDataRowPadding = 4.dp
 val sunSetRiseIconSize = 30.dp
+
+/* Weather Details dimension */
+val detailSurfaceCorner = 14.dp
+val detailListPadding = 2.dp
+val detailListContentPadding = 1.dp
+
+/* WeatherDetailRow dimensions */
+val detailRowSurfacePadding = 3.dp
+val detailRowSurfaceCornerSize = 6.dp
+val detailRowDataPadding = 6.dp
+val detailRowDescTextPadding = 4.dp
