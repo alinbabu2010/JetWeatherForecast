e8c1029f94d8ebdc23bee66aa45408fecbae75b8
Alin Babu <alinbabu2010@gmail.com>
2022-05-17 12:44:14 +0530

    Add variable to get saved temperature unit from dataStore in application class

--- app/src/main/java/com/compose/jetweatherforecast/WeatherApplication.kt
+++ app/src/main/java/com/compose/jetweatherforecast/WeatherApplication.kt
@@ -1,8 +1,36 @@
 package com.compose.jetweatherforecast
 
 import android.app.Application
+import com.compose.jetweatherforecast.utils.Constants
+import com.compose.jetweatherforecast.utils.DataStoreManager
+import com.compose.jetweatherforecast.utils.TemperatureUnit
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltAndroidApp
 class WeatherApplication : Application() {
+
+    @Inject
+    lateinit var dataStoreManager: DataStoreManager
+
+    companion object {
+        var temperatureUnit = Constants.UNIT_IMPERIAL
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        CoroutineScope(Dispatchers.IO).launch {
+            dataStoreManager.unit.distinctUntilChanged().collect {
+                temperatureUnit = when (it) {
+                    TemperatureUnit.Imperial -> Constants.UNIT_IMPERIAL
+                    TemperatureUnit.Metric -> Constants.UNIT_METRIC
+                }
+            }
+        }
+    }
+
 }
