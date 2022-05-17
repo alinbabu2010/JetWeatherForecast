e1b298d694cf35eb8df89481db251be241f0bf41
Alin Babu <alinbabu2010@gmail.com>
2022-05-17 12:29:10 +0530

    Modify and add constant values

--- app/src/main/java/com/compose/jetweatherforecast/utils/Constants.kt
+++ app/src/main/java/com/compose/jetweatherforecast/utils/Constants.kt
@@ -8,8 +8,9 @@ object Constants {
     const val PARAM_UNITS = "units"
     const val PARAM_APPID = "appid"
 
-    const val DEFAULT_UNIT = "imperial"
     const val DEFAULT_CITY = "Seattle"
+    const val UNIT_METRIC = "metric"
+    const val UNIT_IMPERIAL = "imperial"
 
     const val NOT_FOUND = "Location not found"
     const val NETWORK_FAILURE = "Network error occurred. Please try later"
@@ -20,5 +21,8 @@ object Constants {
     const val NAV_ARG_CITY = "city"
 
     const val DELIMITER_COMMA = ","
+
+    const val DATASTORE_NAME = "settings"
+    const val PREF_NAME = "degree_metrics"
 
 }
